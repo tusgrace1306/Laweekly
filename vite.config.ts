133272a1a03f596fdac835ed9bae8ecd2cc3ab5a
tusgrace1306 <126218979+tusgrace1306@users.noreply.github.com>
2025-04-15 22:14:39 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path'; // <- thêm dòng này
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'), // <- alias @ tới thư mục src
+    },
+  },
 });
